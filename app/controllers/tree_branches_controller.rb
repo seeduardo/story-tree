f9bb71a75fe77d5fad92b1abeff94a0dae8916d3
seeduardo <romenflatiron@gmail.com>
2019-04-18 16:32:29 +0100

    Refactored form and got single links up on page

--- app/controllers/tree_branches_controller.rb
+++ app/controllers/tree_branches_controller.rb
@@ -1,6 +1,26 @@
 class TreeBranchesController < ApplicationController
 
   def home
+
+  end
+
+  def up
+    @new_branch = TreeBranch.new(sentence: params[:up])
+    # @trunk = TreeBranch.find()
+    redirect_to root_path
+  end
+
+  def new
+    @tree_branch = TreeBranch.new
+  end
+
+  def show
+    @tree_branch = TreeBranch.find(params[:id])
+  end
+
+  def create
+
+
   end
 
 
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root 'tree_branches#home'
+  post '/up', to: 'tree_branches#up'
 
 end
